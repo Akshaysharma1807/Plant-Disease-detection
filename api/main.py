ea28c279f7f6e8f33d5bcf43cf1e211ed560b10c
Gaurav Sanwal <51902652+gaurav022003@users.noreply.github.com>
2023-05-08 19:31:10 +0530

    Add files via upload

--- api/main.py
+++ api/main.py
@@ -1,1 +1,36 @@
-dhhf
+from fastapi import FastAPI,File,UploadFile
+import uvicorn
+import numpy as np
+from PIL import Image
+from io import BytesIO
+import tensorflow as tf
+import cv2
+
+model=tf.keras.models.load_model("../models/potatoes")
+class_name = ["Early Blight", "Late Blight", "Healthy"]
+def file_image(bytes)->np.ndarray:
+
+    return np.array(Image.open(BytesIO(bytes)))
+
+app = FastAPI()
+
+@app.get("/ping")
+async def ping():
+    return "hello!!"
+
+@app.post("/predict")
+async def predict(
+        file: UploadFile = File(...)
+):
+    img=file_image(await file.read())
+    img=cv2.resize(img, dsize=(256, 256), interpolation=cv2.INTER_CUBIC)
+    img=np.expand_dims(img, 0)
+    prediction=model.predict(img)
+    output=class_name[np.argmax(prediction[0])]
+    return {
+        'class':output,
+        'confidence':float(np.max(prediction[0]))
+    }
+
+if __name__=="__main__":
+    uvicorn.run(app, host='localhost', port=8000)
